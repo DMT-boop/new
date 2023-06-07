7a3142c4dffced4e6ae97ec5e8cbe9d9d3997fee
DMT-boop <135847524+DMT-boop@users.noreply.github.com>
2023-06-07 10:12:26 -0400

    Fichiers initiaux pour un projet MakeCode

---
+++ images.g.ts
@@ -0,0 +1,47 @@
+// Code généré automatiquement. Ne pas modifier.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "test 1":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . 2 2 2 2 2 2 . . . . . 
+. . . . 2 2 . . . . 2 2 . . . . 
+. . . . 2 . f . f . . 2 . . . . 
+. . . . 2 f f . f f . 2 . . . . 
+. . . . 2 . . . . . 2 2 . . . . 
+. . . . 2 2 . . . . 2 . . . . . 
+. . . . . 2 2 2 2 2 2 . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "song2":
+            case "mySong0":return hex`00780004080200`;
+            case "song1":
+            case "mySong":return hex`00780004080200`;
+        }
+        return null;
+    })
+
+}
+// Code généré automatiquement. Ne pas modifier.
